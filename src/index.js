6bab1a3fa3db6e1d77348a191d5cddaed2e9f786
Sumit Sharma <sumitsh.1974@gmail.com>
2025-07-30 23:21:35 +0530

    Fixed a Bug

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="/Think-o-Time">
+    <HashRouter>
     <App />
     </HashRouter>
   </React.StrictMode>
